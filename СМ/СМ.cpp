ae7074d1c23a0a9f46cc255cb2e5fdfa9a87b73d
serge <serge@MAKS1029>
2024-09-25 15:23:42 +0500

    Добавьте файлы проекта.

---
+++ СМ/СМ.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int number, sum = 0;
+
+    cout << "Введите числа (введите 0 для завершения):" << endl;
+
+    while (true) 
+    {
+        cin >> number;
+
+        if (number == 0)
+        {
+            break;
+        }
+
+        sum += number;
+    }
+
+    cout << "Сумма всех чисел: " << sum << endl;
+
+    return 0;
+}
